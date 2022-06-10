f681c67c1e9b66d9c0d964ed12a5b7f290a4873c
Filip Lancaric <filiplancaric222@gmail.com>
2022-06-10 21:41:21 +0200

    Add class Bets - Players child class

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ class Players(ABC):
         self.n_of_players = number_of_players
         self.p_user_order = player_user_order
 
+class Bets(Players):
+    def __init__(self, number_of_players, player_user_order):
+        super().__init__(number_of_players, player_user_order)  
+
 # players = Players(4, random.choice(range(1, 4)))
 
 
