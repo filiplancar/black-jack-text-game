5c266608eba7ba3433d1a5718954483e68c77b7b
Filip Lancaric <filiplancaric222@gmail.com>
2022-06-10 21:38:14 +0200

    Make abstractclass from Players

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+from abc import ABC, abstractmethod
 import random
 
-class Players:
+class Players(ABC):
+    @abstractmethod
     def __init__(self, number_of_players, player_user_order):
         self.n_of_players = number_of_players
         self.p_user_order = player_user_order
 
-players = Players(4, random.choice(range(1, 4)))
+# players = Players(4, random.choice(range(1, 4)))
+
+
 
