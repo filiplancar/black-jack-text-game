60bce213c40dc5ceabef7634edd2f44873e7258b
Filip Lancaric <filiplancaric222@gmail.com>
2022-06-18 14:27:47 +0200

    Change position of sum_of_hand

--- main.py
+++ main.py
@@ -89,18 +89,21 @@ class Game(Players):
 
     def playing(self):
         for player in range(1,self.n_of_players+1):
-            player_choice = randint(0,1)
             sum_of_hand = sum(hands_holder[str(player)])
+            player_choice = randint(0,1)
+            
             
             while player_choice != 0 and sum_of_hand < self.max_value:
                 hands_holder[f'{player}'].append(deck_of_cards.pop())
-                sum_of_hand = sum(hands_holder[str(player)])
+                # sum_of_hand = sum(hands_holder[str(player)])
                 player_choice = randint(0,1)
 
-                if sum_of_hand >= self.max_value and 11 in hands_holder[f'{player}']:
+                if sum_of_hand > self.max_value and 11 in hands_holder[f'{player}']:
                     for i, n in enumerate(hands_holder[f'{player}']):
                         if n == 11:
                             hands_holder[f'{player}'][i] = 1
+                        
+                        sum_of_hand = sum(hands_holder[str(player)])
                 
             print(f'{player}.hráč:', end=' ')
             print(sum_of_hand)
