1712961ad30dfb7c8859a8fe3fd4378464252364
Filip Lancaric <filiplancaric222@gmail.com>
2022-06-30 12:04:20 +0200

    Add code from HEAD 24d0b3a to branch master

--- main.py
+++ main.py
@@ -4,201 +4,215 @@ from random import randint, shuffle
 print('-------')
 print('BLACK JACK')
 print('-------')
+
 class Players(ABC):
     @abstractmethod
     def __init__(self, n_of_players, user_order):
         self.n_of_players = n_of_players
         self.user_order = user_order
 
-bets_holder = {}
-class Bets(Players):
-    def __init__(self, n_of_players, user_order, min_bet, max_bet):
-        super().__init__(n_of_players, user_order)  
-        self.min_bet = min_bet
-        self.max_bet = max_bet
-        
-    def betting(self):
-        for player in range(1,self.n_of_players+1):
-            if player != self.user_order:    
-                print(f'{player}. hráč')
-                bets_holder[str(player)] = randint(self.min_bet, self.max_bet)
-                print('stávka(€):', bets_holder[str(player)])
+menu = 0
 
-            else:
-                while 1:
-                    try:
-                        print(f'{player}. hráč(TY)')
-                        bets_holder[str(player)] = int(input('stávka(€): '))
+while menu != 2:
+    print('1.HRAŤ')
+    print('2.KONIEC')
+    print('-------')
+
+    try:
+        menu = int(input('Zvol možnosť: '))
+    except ValueError as ve:
+        print('-------')
+        print(ve)
+        continue
+
+    if menu == 1:
+        bets_holder = {}
+        class Bets(Players):
+            def __init__(self, n_of_players, user_order, min_bet, max_bet):
+                super().__init__(n_of_players, user_order)  
+                self.min_bet = min_bet
+                self.max_bet = max_bet
                         
-                        if bets_holder[str(player)] <= self.max_bet and bets_holder[str(player)] >= self.min_bet:
-                            break
+            def betting(self):
+                for player in range(1,self.n_of_players+1):
+                    if player != self.user_order:    
+                        print(f'{player}. hráč')
+                        bets_holder[str(player)] = randint(self.min_bet, self.max_bet)
+                        print('stávka(€):', bets_holder[str(player)])
 
-                    except ValueError as e:
-                        print('Zle zvolená stávka')
-                        continue
+                    else:
+                        while 1:
+                            try:
+                                print(f'{player}. hráč(TY)')
+                                bets_holder[str(player)] = int(input('stávka(€): '))
+                                
+                                if bets_holder[str(player)] <= self.max_bet and bets_holder[str(player)] >= self.min_bet:
+                                    break
 
-bets = Bets(4, randint(1, 4), 1000, 10000)
-print('STÁVKY')
-print(f'min. stávka: {bets.min_bet}, ' + f'max. stávka: {bets.max_bet}')
-print('-------')
-bets.betting()
-print('-------')
+                            except ValueError as e:
+                                print('Zle zvolená stávka')
+                                continue
 
-deck_of_cards = 4*[2,3,4,5,6,7,8,9,10,10,10,10,11]
-hands_holder = {
-    '1' : [11,9],
-    '2' : [11,3],
-    '3' : [11,10],
-    '4' : [11,10,9],
-    'dealer' : [10,10,9]
-}
+        bets = Bets(4, randint(1, 4), 1000, 10000)
+        print('-------')
+        print('STÁVKY')
+        print(f'min. stávka: {bets.min_bet}, ' + f'max. stávka: {bets.max_bet}')
+        print('-------')
+        bets.betting()
+        print('-------')
 
-class Dealing(Players):
-    def __init__(self, n_of_players, hand, user_order='None'):
-        super().__init__(n_of_players, user_order)    
-        self.hand = hand
-    
-    @staticmethod
-    def shuffle_cards():
-        shuffle(deck_of_cards)
+        deck_of_cards = 4*[2,3,4,5,6,7,8,9,10,10,10,10,11]
+        hands_holder = {}
 
-    def players_hand(self):
-        for player in range(1,self.n_of_players+1):
-            temp = []
-            for card in range(1, self.hand+1):
-                temp.append(deck_of_cards.pop())
-                
-            print(f'{player}.hráč: ' + ', '.join(map(str, temp))) 
-            hands_holder[str(player)] = temp
-    
-    def dealers_hand(self):
-        temp = []
-        for card in range(1, self.hand+1):
-            temp.append(deck_of_cards.pop())
-        
-        print(f'dealer: {temp[0]}, ?')
-        hands_holder['dealer'] = temp
-    
-dealing = Dealing(bets.n_of_players, 2)
-print('DEALOVANIE')
-print('-------')
-dealing.shuffle_cards()
-dealing.players_hand()
-dealing.dealers_hand()
-print('-------')
+        class Dealing(Players):
+            def __init__(self, n_of_players, hand, user_order='None'):
+                super().__init__(n_of_players, user_order)    
+                self.hand = hand
+                    
+            @staticmethod
+            def shuffle_cards():
+                shuffle(deck_of_cards)
 
-class Game(Players):
-    def __init__(self, n_of_players, user_order, max_value):
-        super().__init__(n_of_players, user_order)
-        self.max_value = max_value
+            def players_hand(self):
+                for player in range(1,self.n_of_players+1):
+                    temp = []
+                    for card in range(1, self.hand+1):
+                        temp.append(deck_of_cards.pop())
+                                
+                    print(f'{player}.hráč: ' + ', '.join(map(str, temp))) 
+                    hands_holder[str(player)] = temp
+                    
+            def dealers_hand(self):
+                temp = []
+                for card in range(1, self.hand+1):
+                    temp.append(deck_of_cards.pop())
+                        
+                print(f'dealer: {temp[0]}, ?')
+                hands_holder['dealer'] = temp
+                    
+        dealing = Dealing(bets.n_of_players, 2)
+        print('DEALOVANIE')
+        print('-------')
+        dealing.shuffle_cards()
+        dealing.players_hand()
+        dealing.dealers_hand()
+        print('-------')
 
-    def playing(self):
-        while True:
-            self.sum_of_hand = sum(hands_holder[str(player)])
-            for i, n in enumerate(hands_holder[f'{player}']):
-                if self.sum_of_hand > self.max_value and n == 11:
-                    hands_holder[f'{player}'][i] = 1
-                    self.sum_of_hand = sum(hands_holder[str(player)])    
+        class Game(Players):
+            def __init__(self, n_of_players, user_order, max_value):
+                super().__init__(n_of_players, user_order)
+                self.max_value = max_value
 
-            if self.sum_of_hand>=21:
-                return False
+            def playing(self):
+                while True:
+                    self.sum_of_hand = sum(hands_holder[str(player)])
+                    for i, n in enumerate(hands_holder[str(player)]):
+                        if self.sum_of_hand > self.max_value and n == 11:
+                            hands_holder[f'{player}'][i] = 1
+                            self.sum_of_hand = sum(hands_holder[str(player)])    
 
-            if player != self.user_order:
-                player_choice = randint(0,1)        
+                    if self.sum_of_hand>=21:
+                        return False
+
+                    if player == self.user_order:
+                        print()
+                        print('TVOJE KARTY:', ', '.join(map(str,hands_holder[str(player)])))
+                        try:
+                            player_choice = int(input('Ďalšia karta? 0=NIE/1=ANO: '))  
+
+                        except Exception as e:
+                            print(e)
+                            continue
+                                        
+                    else:
+                        player_choice = randint(0,1)                   
+
+                    if player_choice == 0:
+                        return False
+                                    
+                    hands_holder[f'{player}'].append(deck_of_cards.pop())
+
+            @staticmethod  
+            def print_hands():
+                print(f'{player}.hráč:', end=' ')
+                print(', '.join(map(str,hands_holder[str(player)])))
+
+            def dealer_playing(self, d_min_value):
+                self.d_min_value = d_min_value
+                self.sum_of_hand = sum(hands_holder['dealer'])
+
+                while True:
+                    self.sum_of_hand = sum(hands_holder['dealer'])
+
+                    for i, n in enumerate(hands_holder['dealer']):
+                        if self.sum_of_hand > self.max_value and n == 11:
+                            hands_holder['dealer'][i] = 1
+                            self.sum_of_hand = sum(hands_holder['dealer'])   
+                            
+                    if self.sum_of_hand > self.d_min_value-1:
+                        return False
+                            
+                    hands_holder['dealer'].append(deck_of_cards.pop())
+                    
+            @staticmethod
+            def print_dealer():
+                print(f'dealer:', end=' ')
+                print(', '.join(map(str,hands_holder['dealer'])))
+
+        game = Game(bets.n_of_players, bets.user_order, 21)
+        print('ŤAHANIE KARIET')
+        print('-------')
+        for player in range(1,dealing.n_of_players+1):
+            game.playing()
+            game.print_hands()
             
-            else:
-                print()
-                print('TVOJE KARTY:', ', '.join(map(str,hands_holder[str(player)])))
-                try:
-                    player_choice = int(input('Ďalšia karta? 0=NIE/1=ANO: '))  
+        game.dealer_playing(17)
+        game.print_dealer()
+        print('-------')
 
-                except Exception as e:
-                    print(e)
-                    continue
+        class Results(Game):
+            def __init__(self, n_of_players, user_order, max_value):
+                super().__init__(n_of_players, user_order, max_value)
 
-            if player_choice == 0:
-                return False
-            
-            hands_holder[f'{player}'].append(deck_of_cards.pop())
+            ace = ([10,11], [11,10])
+            def comparison(self):
+                for player in range(1,results.n_of_players+1):
+                    if sum(hands_holder['dealer']) <= self.max_value:
+                        if (sum(hands_holder[str(player)]) > sum(hands_holder['dealer']) and sum(hands_holder[str(player)]) <= self.max_value):
+                            if hands_holder[str(player)] in self.ace:
+                                print(f'{player}. hráč vyhral:', 1.5*bets_holder[str(player)], '€')
+                            else:
+                                print(f'{player}. hráč vyhral', bets_holder[str(player)], '€')
+                                
+                        elif (sum(hands_holder[str(player)]) < sum(hands_holder['dealer'])) or (sum(hands_holder[str(player)]) > self.max_value):
+                            print(f'{player}. Dealer vyhral', bets_holder[str(player)], '€')
+                                
+                        elif sum(hands_holder[str(player)]) == sum(hands_holder['dealer']):
+                            if hands_holder['dealer'] in self.ace and bets_holder[str(player)] not in self.ace:
+                                print(f'{player}. Dealer vyhral', bets_holder[str(player)], '€')
+                                    
+                            elif hands_holder[str(player)] in self.ace and hands_holder['dealer'] not in self.ace:
+                                print(f'{player}. hráč vyhral:', 1.5*bets_holder[str(player)], '€')
 
-    @staticmethod  
-    def print_hands():
-        print(f'{player}.hráč:', end=' ')
-        print(', '.join(map(str,hands_holder[str(player)])))
+                            else: 
+                                print(f'{player}. Remíza(Peniaze nevyhráva ani jeden)')
 
-    def dealer_playing(self, d_min_value):
-        self.d_min_value = d_min_value
-        self.sum_of_hand = sum(hands_holder['dealer'])
+                    else:
+                        if sum(hands_holder[str(player)]) > self.max_value:
+                            print(f'{player}. Dealer vyhral', bets_holder[str(player)], '€')
+                            
+                        else:
+                            if hands_holder[str(player)] in self.ace and hands_holder['dealer'] not in self.ace:
+                                print(f'{player}. hráč vyhral:', 1.5*bets_holder[str(player)], '€')
+                            else:
+                                print(f'{player}. hráč vyhral', bets_holder[str(player)], '€')
 
-        while True:
-            self.sum_of_hand = sum(hands_holder['dealer'])
+        results = Results(game.n_of_players, game.user_order, 21)
+        print('VÝSLEDKY')
+        print('-------')
+        results.comparison()
 
-            for i, n in enumerate(hands_holder['dealer']):
-                if self.sum_of_hand > self.max_value and n == 11:
-                    hands_holder['dealer'][i] = 1
-                    self.sum_of_hand = sum(hands_holder['dealer'])   
-            
-            if self.sum_of_hand > self.d_min_value-1:
-                return False
-            
-            hands_holder['dealer'].append(deck_of_cards.pop())
-    
-    @staticmethod
-    def print_dealer():
-        print(f'dealer:', end=' ')
-        print(', '.join(map(str,hands_holder['dealer'])))
-
-game = Game(bets.n_of_players, bets.user_order, 21)
-print('ŤAHANIE KARIET')
-print('-------')
-for player in range(1,game.n_of_players+1):
-    game.playing()
-    game.print_hands()
-
-game.dealer_playing(17)
-game.print_dealer()
-print('-------')
-
-class Results(Game):
-    def __init__(self, n_of_players, user_order, max_value):
-        super().__init__(n_of_players, user_order, max_value)
-
-    def comparison(self):
-        if sum(hands_holder['dealer']) <= self.max_value:
-            if sum(hands_holder[str(player)]) >= sum(hands_holder['dealer']) and sum(hands_holder[str(player)]) <= self.max_value:
-                if sum(hands_holder[str(player)]) == self.max_value and len(hands_holder[str(player)])==2 and len(hands_holder['dealer']) > 2:
-                    print(f'{player}. hráč vyhral:', 1.5*bets_holder[str(player)])
-                elif sum(hands_holder[str(player)]) > sum(hands_holder['dealer']):
-                    print(f'{player}. hráč vyhral:', bets_holder[str(player)])
-                elif sum(hands_holder[str(player)]) == sum(hands_holder['dealer']):
-                    print(f'{player}. Remíza(Peniaze nevyhráva ani jeden)')    
-        
-            elif (sum(hands_holder[str(player)]) < sum(hands_holder['dealer'])) or (sum(hands_holder[str(player)]) > self.max_value):
-                print(f'{player}. Dealer vyhral:', bets_holder[str(player)])
-            
-        
-
-        else:
-            if sum(hands_holder[str(player)]) > self.max_value:
-                print(f'{player}. Dealer vyhral:', bets_holder[str(player)])
-            
-            elif sum(hands_holder[str(player)]) == self.max_value and len(hands_holder[str(player)])==2:
-                print(f'{player}. hráč vyhral:', 1.5*bets_holder[str(player)])
-            
-            else:
-                print(f'{player}. hráč vyhral:', bets_holder[str(player)])
-    
-results = Results(game.n_of_players, game.user_order, game.max_value)
-print('VÝSLEDKY')
-print('-------')
-for player in range(1,results.n_of_players+1):
-    results.comparison()
-
-print(bets_holder)
-print('-------')
-
-
-
-
-
-
+        print()
+        print('--------------')
+        print('--------------')
+        print()
